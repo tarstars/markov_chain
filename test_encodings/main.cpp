bdfdb5eb0da5f49bcc2ad327a6f748546bf4a9ad
Arseniy Trushin <142857@mail.ru>
2019-03-01 20:01:42 +0300

    coding test works

--- test_encodings/main.cpp
+++ test_encodings/main.cpp
@@ -6,7 +6,9 @@
 using namespace std;
 
 void testReadStringFromFile() {
-    // std::setlocale(LC_ALL, "ru_RU.utf8");
+    std::setlocale(LC_ALL, "ru_RU.utf8");
+
+    /*
     std::string s1 = "t11й11";
     std::cout << "ordinary string " << s1 << std::endl;
     for (size_t ind = 0; ind < s1.size(); ++ind) {
@@ -14,18 +16,21 @@ void testReadStringFromFile() {
                   << std::boolalpha << (bool)std::isalpha(static_cast<unsigned char>(s1[ind])) << std::endl;
     }
     std::cout << std::endl;
+    */
     std::ifstream source("utf8.txt");
     std::string s2;
     if (!(std::getline(source, s2))) {
         std::cerr << "file read error" << std::endl;
         return;
     }
+    /*
     std::cout << "from file: s2 = " << s2 << std::endl;
     for (size_t ind = 0; ind < s2.size(); ++ind) {
         std::cout << s2[ind] << " "
                   << std::boolalpha << (bool)std::isalpha(static_cast<unsigned char>(s2[ind])) << std::endl;
     }
     std::cout << std::endl;
+    */
 
     std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
     std::wstring ws = converter.from_bytes(s2);
@@ -42,6 +47,15 @@ void testReadStringFromFile() {
     for (size_t ind = 0; ind < ws2.size(); ++ind) {
         std::wcout << ws2[ind] << " "
                   << std::boolalpha << (bool)std::isalpha(ws2[ind], loc) << std::endl;
+    }
+    std::cout << std::endl;
+
+    // std::wcout.imbue(loc);
+    std::wstring ws3 = L"1 мама !";
+    std::wcout << "ws3 = " << ws3 << std::endl;
+    for (size_t ind = 0; ind < ws3.size(); ++ind) {
+        std::wcout << ws3[ind] << " "
+                   << std::boolalpha << (bool)std::isalpha(ws3[ind], loc) << std::endl;
     }
     std::cout << std::endl;
 }
